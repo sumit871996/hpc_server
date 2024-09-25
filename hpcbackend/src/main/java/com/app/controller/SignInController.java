ea948ba484d845f20d5b050854f8da5c4877d3bd
Sumit Mandlik <mandlik.sumit871996@gmail.com>
2024-09-25 14:44:22 +0530

    test

--- hpcbackend/src/main/java/com/app/controller/SignInController.java
+++ hpcbackend/src/main/java/com/app/controller/SignInController.java
@@ -11,6 +11,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -42,6 +43,12 @@ public class SignInController {
 	
 	@Autowired
 	private UserServiceImpl userService;
+	
+	@GetMapping("/test")
+	public ResponseEntity<?> testApp()
+	{
+		return ResponseEntity.status(HttpStatus.OK).body("application running properly");
+	}
 	
 	@PostMapping("/signin")
 	public ResponseEntity<?> validateUserCreateToken(@RequestBody @Valid AuthRequest request) {
